c25f9f6d79d0919935885da6461e9158f94bb69d
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-18 07:35:54 +0000

    constants and shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,4 +6,15 @@ fn main() {
     println!("x is equal to {}",x);
     x = 6;
     println!("x is equal to {}",x);
+
+    //constants
+    // convention is to have uppercase with underscores
+    // rather than commas, you can use underscores for readability
+    const MAX_POINTS: u32 = 100_000;
+
+    // shadowing
+    let x = 5;
+    let x = x + 1;
+    let x = x*2;
+    println!("x is equal to {}",x);
 }
